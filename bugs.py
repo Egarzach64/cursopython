628f49b5d932b7532467c08ba297d76b9217c3dc
Eduardo Garza <eduardo.garza@cydsa.com>
2023-01-27 16:35:00 -0600

    random 1 a 20 con letrero

--- bugs.py
+++ bugs.py
@@ -1,7 +1,7 @@
 import random
 numero1 = random.randint(1, 20)
 numero2 = random.randint(1, 20)
-print('¿Cuánto es ' + str(numero1) + ' + ' + str(numero2) + '?')
+print('¿Cuánto es la suma de ' + str(numero1) + ' + ' + str(numero2) + '?')
 respuesta = input()
 if int(respuesta) == numero1 + numero2:
    print('¡Correcto!')
