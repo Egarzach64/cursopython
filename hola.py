7dc6509f51e3f4aa18a834bf51620a20f7291b0e
Eduardo Garza <eduardo.garza@cydsa.com>
2023-01-30 11:48:33 -0600

    gitignore1

--- hola.py
+++ hola.py
@@ -1,5 +1,5 @@
 # Este Programa Saluda y Pregunta x mi nombre
-print("Hola Mundo aqui no se actualiza")
+print("Hola Mundo aqui no se actualiza ni me pelo")
 print("Como te llamas?")
 minombre = input()
 print("Es un placer conocerte," + minombre)
